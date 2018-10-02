257f38400a5e61a2d32b9ccf90c1d57ae262876b
guenneug <ln.guenneugues@wanadoo.fr>
2018-10-02 16:50:25 +0200

    Formulaire de saisie des clients + layout resumeLocationActivity + clientDAO

--- app/src/main/java/fr/eni/lokacar/DetailVehiculeActivity.java
+++ app/src/main/java/fr/eni/lokacar/DetailVehiculeActivity.java
@@ -2,6 +2,7 @@ package fr.eni.lokacar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DetailVehiculeActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class DetailVehiculeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_vehicule);
+    }
+
+    public void createLocation(View view) {
+
     }
 }
