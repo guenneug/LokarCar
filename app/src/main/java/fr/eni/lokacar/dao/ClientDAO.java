67f0751655903ebdbe3dd103b62facd7afab3fd3
cmenerville2018 <claude.menerville@gmail.Com>
2018-10-03 14:27:19 +0200

    Mise a jour des DAO. (supression des voitures

--- app/src/main/java/fr/eni/lokacar/dao/ClientDAO.java
+++ app/src/main/java/fr/eni/lokacar/dao/ClientDAO.java
@@ -1,13 +1,6 @@
 package fr.eni.lokacar.dao;
 
 import android.arch.persistence.room.Dao;
-import android.arch.persistence.room.Delete;
-import android.arch.persistence.room.Insert;
-import android.arch.persistence.room.Query;
-import android.arch.persistence.room.Update;
-import android.content.Context;
-
-import java.util.List;
 
 import fr.eni.lokacar.bo.Client;
 
--- app/src/main/java/fr/eni/lokacar/dao/PhotoDAO.java
+++ app/src/main/java/fr/eni/lokacar/dao/PhotoDAO.java
@@ -1,12 +1,6 @@
 package fr.eni.lokacar.dao;
 
 import android.arch.persistence.room.Dao;
-import android.arch.persistence.room.Delete;
-import android.arch.persistence.room.Insert;
-import android.arch.persistence.room.Update;
-
-import java.util.List;
-
 import fr.eni.lokacar.bo.Photo;
 
 @Dao
