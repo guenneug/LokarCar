d027075864fb1ba1a7f76fa457d4188cb92b5a21
christopheMorel2018 <christophemorel35@gmail.com>
2018-10-03 10:31:33 +0200

    Suppression de l'enum et création d'une classe TypeVehicule

--- app/src/main/java/fr/eni/lokacar/bo/TypeVehicule.java
+++ app/src/main/java/fr/eni/lokacar/bo/TypeVehicule.java
@@ -1,6 +1,38 @@
 package fr.eni.lokacar.bo;
 
-public enum TypeVehicule {
+public class TypeVehicule {
 
- BERLINE, CABRIOLET, BREAK, SUV, MONOSPACE, COUPE
+    private int id;
+    private String libelle;
+
+    public TypeVehicule() {
+    }
+
+    public TypeVehicule(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    @Override
+    public String toString() {
+        return "TypeVehicule{" +
+                "id=" + id +
+                ", libelle='" + libelle + '\'' +
+                '}';
+    }
 }
