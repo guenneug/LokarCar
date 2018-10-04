e78fbed2e9dc39419b238a068876e3cdcaf304e4
christopheMorel2018 <christophemorel35@gmail.com>
2018-10-04 13:35:14 +0200

    Recherche client par nom

--- app/src/main/java/fr/eni/lokacar/ListClientActivity.java
+++ app/src/main/java/fr/eni/lokacar/ListClientActivity.java
@@ -5,11 +5,13 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import fr.eni.lokacar.adapters.ClientAdapter;
@@ -22,6 +24,8 @@ public class ListClientActivity extends AppCompatActivity implements ClientAdapt
     private static String KEY_AGENCE = "nomAgence";
     private static String KEY_NOM_FICHIER = "fichierAgence";
 
+    private static final String TAG = "CHRISTOPHE";
+
     private android.support.v7.widget.Toolbar toolbar;
     private RecyclerView recyclerView;
     private EditText etSearchClient;
@@ -32,17 +36,24 @@ public class ListClientActivity extends AppCompatActivity implements ClientAdapt
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_client);
 
+        initComponent();
+
+    }
+
+
+    public void initComponent()
+    {
         this.toolbar = findViewById(R.id.tb_ListClients);
         SharedPreferences sp = this.getSharedPreferences(KEY_NOM_FICHIER,MODE_PRIVATE);
-
         String nomAgence = sp.getString(KEY_AGENCE,"Nom agence");
         this.toolbar.setTitle(nomAgence);
-
         this.setSupportActionBar(toolbar);
 
         this.recyclerView= findViewById(R.id.rv_clients);
-        //pr ke ttes cardview même taille
         recyclerView.setHasFixedSize(true);
+
+        this.etSearchClient=findViewById(R.id.etRechercheClient);
+
 
 
     }
@@ -51,7 +62,14 @@ public class ListClientActivity extends AppCompatActivity implements ClientAdapt
     protected void onResume() {
         super.onResume();
 
-        List<Client> clients = Connexion.getConnexion(this).clientDAO().selectAll();
+        List<Client> clients = new ArrayList<>();
+
+        if(this.etSearchClient == null || this.etSearchClient.getText().toString().length() == 0)
+        {
+            clients = Connexion.getConnexion(this).clientDAO().selectAll();
+
+        }
+
         this.clientAdapter = new ClientAdapter(this);
 
         this.clientAdapter.addClients(clients);
@@ -86,10 +104,24 @@ public class ListClientActivity extends AppCompatActivity implements ClientAdapt
     }
 
     public void onClickSearchClient(View view) {
+
+        List<Client> clientsRecherches = new ArrayList<>();
+
+        String recherche = etSearchClient.getText().toString();
+
+        Log.i(TAG,"Recherche : "+recherche);
+
+        clientsRecherches=Connexion.getConnexion(this).clientDAO().findByName(recherche);
+        this.clientAdapter = new ClientAdapter(this);
+
+        this.clientAdapter.addClients(clientsRecherches);
+        this.recyclerView.setAdapter(clientAdapter);
+
     }
 
     @Override
     public void onClickClient(Client client) {
+
         Intent intent = new Intent(this, ResumeLocationActivity.class);
         intent.putExtra("client", client);
         startActivity(intent);
